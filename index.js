847045ee8694a2c02b9bb1a3b25dae3b5137135d
Jamie Nadel <jamienadel4@gmail.com>
2023-03-22 10:59:15 -0400

    added questions array

--- index.js
+++ index.js
@@ -3,7 +3,43 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?'
+  },
+  {
+    type: 'input',
+    name: 'contents',
+    message: 'What would you like add to table of contents?'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What would you like add to the installation section?'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What would you like add to the usage section?'
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'What would you like add to the license section?'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What would you like add to the contributing section?'
+  },
+  {
+    type: 'input',
+    name: 'questions',
+    message: 'What would you like add to the questions section?'
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
